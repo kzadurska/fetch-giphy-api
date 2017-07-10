6c4c4e9c85c604bd054c5bcb7c759e57dacd58a1
Kasia Zadurska <kzadurska@gmail.com>
2017-07-10 21:23:17 +0200

    Use es6 class syntax

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,31 +1,48 @@
-var resultsList = document.getElementsByTagName('ul')[0];
-var form = document.getElementsByTagName('form')[0];
+class GiphySearch {
+	static form() {
+		return document.getElementsByTagName('form')[0]
+	}
 
+	static resultsList() {
+		return document.getElementsByTagName('ul')[0]
+	}
 
-form.addEventListener('submit', function(event){
-	event.preventDefault();
-	var searchQuery = escape(form.query.value);
-	var giphyQuery = "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=7c48f0399916458a8873ecfb12ccb14d&limit=10";
+	constructor() {
+		this.listen = this.listen.bind(this)
+	}
 
-	fetch(giphyQuery)
+	listen() {
+		GiphySearch.form().addEventListener('submit', this.submitForm)
+	}
+
+	submitForm(event) {
+		event.preventDefault();
+		const searchQuery = escape(GiphySearch.form().query.value)
+		
+		const giphyQuery = "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=7c48f0399916458a8873ecfb12ccb14d&limit=10"
+		fetch(giphyQuery)
 		.then(function(response) {
 			if(response.ok) {
 				return response.json().then(function(json) {
-				    for(i = 0; i < json.data.length; i++) {
-				      	var listItem = document.createElement('li');
-				      	listItem.innerHTML = '<video><source src="'+json.data[i].images.fixed_width.mp4+'" type="video/mp4"></video>'; 
-				      	resultsList.appendChild(listItem);
-				    };
-				});
+				    for(let i = 0; i < json.data.length; i++) {
+				      	const listItem = document.createElement('li')
+				      	listItem.innerHTML = '<video><source src="'+json.data[i].images.fixed_width.mp4+'" type="video/mp4"></video>' 
+				      	GiphySearch.resultsList().appendChild(listItem)
+				    }
+				})
 			} else {
-				var responseMessage = document.createElement('p');
-				responseMessage.innerText = "Network response was not ok.";
-				document.body.appendChild(responseMessage);
+				const responseMessage = document.createElement('p')
+				responseMessage.innerText = "Network response was not ok."
+				document.body.appendChild(responseMessage)
 			}
 			})
 		.catch(function(error) {
-				var errorMessage = document.createElement('p');
-				errorMessage.innerText = "There has been a problem with your fetch operation: " + error.message;
-				document.body.appendChild(errorMessage);
+				const errorMessage = document.createElement('p')
+				errorMessage.innerText = "There has been a problem with your fetch operation: " + error.message
+				document.body.appendChild(errorMessage)
 		});
-});
+	}
+}
+
+const app = new GiphySearch()
+app.listen()
