ec0435ae79e85764afc656a9e5859a58e07afeeb
Kasia Zadurska <kzadurska@gmail.com>
2017-07-10 22:14:13 +0200

    Fix indentation in JS
    Where? Why? Did it get so wrong?

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,47 +1,47 @@
 class GiphySearch {
-	static form() {
-		return document.getElementsByTagName('form')[0]
-	}
+  static form() {
+    return document.getElementsByTagName('form')[0]
+  }
 
-	static resultsList() {
-		return document.getElementsByTagName('ul')[0]
-	}
+  static resultsList() {
+    return document.getElementsByTagName('ul')[0]
+  }
 
-	constructor() {
-		// this.listen = this.listen.bind(this)
-	}
+  constructor() {
+    // this.listen = this.listen.bind(this)
+  }
 
-	listen() {
-		GiphySearch.form().addEventListener('submit', this.submitForm)
-	}
+  listen() {
+    GiphySearch.form().addEventListener('submit', this.submitForm)
+  }
 
-	submitForm(event) {
-		event.preventDefault();
-		const searchQuery = escape(GiphySearch.form().query.value)
-		
-		const giphyQuery = "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=7c48f0399916458a8873ecfb12ccb14d&limit=10"
-		fetch(giphyQuery)
-		.then(function(response) {
-			if(response.ok) {
-				return response.json().then(function(json) {
-			    for (const gif of json.data) {
-			    	const listItem = document.createElement('li')
-			    	listItem.innerHTML = '<video><source src="'+gif.images.fixed_width.mp4+'" type="video/mp4"></video>' 
-			      GiphySearch.resultsList().appendChild(listItem)
-			    }
-				})
-			} else {
-				const responseMessage = document.createElement('p')
-				responseMessage.innerText = "Network response was not ok."
-				document.body.appendChild(responseMessage)
-			}
-			})
-		.catch(function(error) {
-				const errorMessage = document.createElement('p')
-				errorMessage.innerText = "There has been a problem with your fetch operation: " + error.message
-				document.body.appendChild(errorMessage)
-		});
-	}
+  submitForm(event) {
+    event.preventDefault();
+    const searchQuery = escape(GiphySearch.form().query.value)
+    
+    const giphyQuery = "http://api.giphy.com/v1/gifs/search?q=" + searchQuery + "&api_key=7c48f0399916458a8873ecfb12ccb14d&limit=10"
+    fetch(giphyQuery)
+    .then(function(response) {
+      if(response.ok) {
+        return response.json().then(function(json) {
+          for (const gif of json.data) {
+            const listItem = document.createElement('li')
+            listItem.innerHTML = '<video><source src="'+gif.images.fixed_width.mp4+'" type="video/mp4"></video>' 
+            GiphySearch.resultsList().appendChild(listItem)
+          }
+        })
+      } else {
+        const responseMessage = document.createElement('p')
+        responseMessage.innerText = "Network response was not ok."
+        document.body.appendChild(responseMessage)
+      }
+      })
+    .catch(function(error) {
+        const errorMessage = document.createElement('p')
+        errorMessage.innerText = "There has been a problem with your fetch operation: " + error.message
+        document.body.appendChild(errorMessage)
+    });
+  }
 }
 
 const app = new GiphySearch()
